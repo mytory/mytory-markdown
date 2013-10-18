9a56dff3dd508e688db17e201cc423179b79e996
mytory <mytory@gmail.com>
2013-10-19 05:00:50 +0900

    숏코드로 마크다운 파일을 넣던 걸 바꿔서 postmeta로 파일 URL을 넣는 것으로 했다.
    1. post_content에 실제 내용이 들어가야 워드프레스 검색이 작동할 거다.
    2. post_content에 실제 내용이 들어가야 다른 플러그인들과의 호환성을 간단히 보장할 수 있다.
    
    이런 이유로 그렇게 했다.

--- main.php
+++ main.php
@@ -11,8 +11,10 @@ class Mytory_Markdown {
 
     var $file_not_found;
 
-    function Mytory_Markdown(){
-        add_shortcode( 'mytory-md', array(&$this, 'apply_markdown'));
+    function Mytory_Markdown() {
+        add_filter('the_content', array(&$this, 'apply_markdown'));
+        add_action('add_meta_boxes', array(&$this, 'register_meta_box'));
+        add_action('save_post', array(&$this, 'update_post'));
     }
 
     /**
@@ -20,38 +22,44 @@ class Mytory_Markdown {
      * @param  array $attr shortcod attributes
      * @return string contents markdown apply
      */
-    public function apply_markdown( $attr ){
+    public function apply_markdown($post_content, $post_id = NULL) {
         global $post;
 
-        $attr['path'] = str_replace('https://', 'http://', $attr['path']);
+        if( ! is_single()){
+            return $post_content;
+        }
 
-        if($this->_need_to_save($attr['path'])){
+        if($post_id){
+            $post = get_post($post_id);
+        }
 
-            if($this->file_not_found === TRUE){
-                return "<p>Markdown file is not found.</p>";
-            }
+        $markdown_path = get_post_meta($post->ID, 'mytory_md_path', TRUE);
+        $markdown_path = str_replace('https://', 'http://', $markdown_path);
 
-            update_post_meta($post->ID, '_mytory_markdown_etag', $this->_get_etag($attr['path']));
-            $md_content = $this->_file_get_contents($attr['path']);
+        if ($this->_need_to_save($markdown_path)) {
 
-            if( ! function_exists('Markdown')){
+            update_post_meta($post->ID, '_mytory_markdown_etag', $this->_get_etag($markdown_path));
+            $md_content = $this->_file_get_contents($markdown_path);
+
+            if (!function_exists('Markdown')) {
                 include_once 'markdown.php';
             }
 
-            $html_content = Markdown($md_content);
-            $html_content = preg_replace('/<h1>(.*)<\/h1>/', '', $html_content);
-            $html_content = wptexturize($html_content);
-            $html_content = convert_smilies($html_content);
-            $html_content = convert_chars($html_content);
-            // $html_content = wpautop($html_content);
-            $html_content = shortcode_unautop($html_content);
-            $html_content = prepend_attachment($html_content);
-            update_post_meta($post->ID, 'mytory_markdown_html', $html_content);
-            return $html_content;
-            
-        }else{
-            return get_post_meta($post->ID, 'mytory_markdown_html', true);
+            $post_content = Markdown($md_content);
+            $post_content = preg_replace('/<h1>(.*)<\/h1>/', '', $post_content);
+
+            $postarr = array(
+                'ID' => $post->ID,
+                'post_content' => $post_content,
+            );
+            wp_update_post($postarr);
         }
+
+        if ($this->file_not_found === TRUE AND current_user_can('edit_posts')) {
+            return "<p>Markdown file is not found.</p>" . $post_content;
+        }
+
+        return $post_content;
     }
 
     /**
@@ -60,32 +68,32 @@ class Mytory_Markdown {
      * @param  string $url
      * @return boolean
      */
-    private function _need_to_save($url){
+    private function _need_to_save($url) {
         global $post;
 
         // If not single page, don't connect for prevent time-wasting.
         // return FALSE that means 'no need to save' to print HTML that is saved.
         // 싱글 페이지가 아니라면 굳이 접속해서 시간낭비할 거 없이 
         // 바로 저장된 HTML을 뿌려줄 수 있도록 save할 필요 없다고 신호를 준다.
-        if( ! is_single()){
+        if (!is_single()) {
             return FALSE;
         }
-        
-        $etag_saved = get_post_meta($post->ID, '_mytory_markdown_etag', true);
 
-        if($etag_saved){
+        $etag_saved = get_post_meta($post->ID, '_mytory_markdown_etag', TRUE);
+
+        if ($etag_saved) {
             $etag_remote = $this->_get_etag($url);
 
             // if there is not etag, don't need to save.
-            if($etag_remote === NULL){
-                return false;
+            if ($etag_remote === NULL) {
+                return FALSE;
             }
-            
+
             // if etag different each other, need to save
             return ($etag_saved != $etag_remote);
-        }else{
+        } else {
             // if no cache, need to save
-            return true;
+            return TRUE;
         }
     }
 
@@ -94,17 +102,17 @@ class Mytory_Markdown {
      * @param  string $url
      * @return string
      */
-    private function _get_etag($url){
+    private function _get_etag($url) {
         $header = $this->_get_header_from_url($url);
         $header = $this->_http_parse_headers($header);
 
-        if($header[0] == 'HTTP/1.1 404 NOT FOUND'){
+        if ($header[0] == 'HTTP/1.1 404 NOT FOUND') {
             $this->file_not_found = TRUE;
         }
-        
-        if(isset($header['etag'])){
+
+        if (isset($header['etag'])) {
             return $header['etag'];
-        }else{
+        } else {
             return NULL;
         }
     }
@@ -114,13 +122,14 @@ class Mytory_Markdown {
      * @param  string $url dropbox public url
      * @return string
      */
-    private function _get_header_from_url($url){
+    private function _get_header_from_url($url) {
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_HEADER, true);
-        curl_setopt($curl, CURLOPT_NOBODY, true);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($curl, CURLOPT_HEADER, TRUE);
+        curl_setopt($curl, CURLOPT_NOBODY, TRUE);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, TRUE);
+
         return curl_exec($curl);
     }
 
@@ -129,12 +138,13 @@ class Mytory_Markdown {
      * @param  string $url dropbox public url
      * @return string
      */
-    private function _file_get_contents($url){
+    private function _file_get_contents($url) {
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_HEADER, false);
-        curl_setopt($curl, CURLOPT_NOBODY, false);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HEADER, FALSE);
+        curl_setopt($curl, CURLOPT_NOBODY, FALSE);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+
         return curl_exec($curl);
     }
 
@@ -144,44 +154,84 @@ class Mytory_Markdown {
      * @param  string $raw_headers
      * @return array
      */
-    private function _http_parse_headers($raw_headers)
-    {
+    private function _http_parse_headers($raw_headers) {
         $headers = array();
         $key = ''; // [+]
 
-        foreach(explode("\n", $raw_headers) as $i => $h)
-        {
+        foreach (explode("\n", $raw_headers) as $i => $h) {
             $h = explode(':', $h, 2);
 
-            if (isset($h[1]))
-            {
+            if (isset($h[1])) {
                 if (!isset($headers[$h[0]]))
                     $headers[$h[0]] = trim($h[1]);
-                elseif (is_array($headers[$h[0]]))
-                {
+                elseif (is_array($headers[$h[0]])) {
                     // $tmp = array_merge($headers[$h[0]], array(trim($h[1]))); // [-]
                     // $headers[$h[0]] = $tmp; // [-]
                     $headers[$h[0]] = array_merge($headers[$h[0]], array(trim($h[1]))); // [+]
-                }
-                else
-                {
+                } else {
                     // $tmp = array_merge(array($headers[$h[0]]), array(trim($h[1]))); // [-]
                     // $headers[$h[0]] = $tmp; // [-]
                     $headers[$h[0]] = array_merge(array($headers[$h[0]]), array(trim($h[1]))); // [+]
                 }
 
                 $key = $h[0]; // [+]
-            }
-            else // [+]
+            } else // [+]
             { // [+]
                 if (substr($h[0], 0, 1) == "\t") // [+]
-                    $headers[$key] .= "\r\n\t".trim($h[0]); // [+]
+                $headers[$key] .= "\r\n\t" . trim($h[0]); // [+]
                 elseif (!$key) // [+]
-                    $headers[0] = trim($h[0]);trim($h[0]); // [+]
+                $headers[0] = trim($h[0]);
+                trim($h[0]); // [+]
             } // [+]
         }
 
         return $headers;
+    }
+
+    function register_meta_box() {
+        add_meta_box(
+            'mytory-markdown-path',
+            'Markdown File Path',
+            array(&$this, 'meta_box_inner')
+        );
+    }
+
+    function meta_box_inner() {
+        $markdown_path = get_post_meta($_GET['post'], 'mytory_md_path', TRUE);
+        ?>
+        <table class="form-table">
+            <tr>
+                <th scope="row">URL</th>
+                <td>
+                    <input type="url" name="mytory_md_path" id="mytory-md-path" class="large-text" value="<?php echo $markdown_path?>">
+                </td>
+            </tr>
+            <!-- <tr>
+                <th>Function</th>
+                <td>
+                    <button type="button" class="js-update-content">Update Content</button>
+                </td>
+            </tr> -->
+        </table>
+        <script type="text/javascript">
+            // jQuery(document).ready(function($){
+            //     $('.js-update-content').click(function(){
+
+            //     });
+            // });
+        </script>
+    <?
+    }
+
+    function update_post($post_id) {
+        if (!current_user_can('edit_post', $post_id)) {
+            return;
+        }
+
+        // 데이터 저장
+        if(isset($_POST['mytory_md_path'])){
+            update_post_meta($post_id, 'mytory_md_path', $_POST['mytory_md_path']);
+        }
     }
 }
 
